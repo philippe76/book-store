cfb6e3754ae4a25f1d3ec23686b5f3168b8bb68b
Philippe <soufiane92230@gmail.com>
2024-03-20 15:18:15 +0100

    Add storage books file

---
+++ storage.js
@@ -0,0 +1,39 @@
+let books = [
+    {
+        title: `Eloquent JavaScript`,
+        author: `Marijn Haverbeke`,
+        year: 2018
+    },
+    {
+        title: `You Don't Know JS`,
+        author: `Kyle Simpson`,
+        year: 2015
+    },
+    {
+        title: `The Book With No Name`,
+        author: `Anonymous`,
+        year: 2010
+    },
+    {
+        title: `1984`,
+        author: `George Orwell`,
+        year: 1949
+    },
+    {
+        title: `Métro 2033`,
+        author: `Dmitri Gloukhovski`,
+        year: 2002
+    },
+    {
+        title: `Fight club`,
+        author: `Chuck Palahniuk`,
+        year: 1996
+    },
+    {
+        title: `Brave New World`,
+        author: `Aldous Huxley`,
+        year: 1932
+    }
+]
+
+export default books;
